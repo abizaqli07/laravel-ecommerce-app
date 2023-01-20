40f8eb6efb512db657ea117573f680c20e516615
abikhoir07 <abikhoir42@gmail.com>
2023-01-20 14:38:58 +0700

    display product by category

--- routes/web.php
+++ routes/web.php
@@ -23,6 +23,7 @@ Auth::routes();
 Route::controller(App\Http\Controllers\Frontend\FrontendController::class)->group(function () {
 	Route::get('/', 'index');
 	Route::get('/collections', 'categories');
+	Route::get('/collections/{category_slug}', 'products');
 });
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
